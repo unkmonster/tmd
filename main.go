5c8bbfb8e87933c80d1357495bb37fe3f64df500
unkmonster <106185616+unkmonster@users.noreply.github.com>
2024-08-22 17:34:48 +0800

    adjustment busy_timeout with sqlite

--- main.go
+++ main.go
@@ -370,7 +370,7 @@ func connectDatabase(path string) (*sqlx.DB, error) {
 		return nil, err
 	}
 
-	dsn := fmt.Sprintf("file:%s?cache=shared&_journal_mode=WAL", path)
+	dsn := fmt.Sprintf("file:%s?cache=shared&_journal_mode=WAL&busy_timeout=10000", path)
 	db, err := sqlx.Connect("sqlite3", dsn)
 	if err != nil {
 		return nil, err
